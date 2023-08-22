afc4d05f3541d71bb09a0395396d45acff4d054c
bugph0bia <64964079+bugph0bia@users.noreply.github.com>
2023-08-22 23:07:15 +0900

    テストコードを追加

---
+++ test.py
@@ -0,0 +1,10 @@
+"""
+Test Procedure:
+  1. Install "chromedriver-binary-auto". (e.g.: `pip install -r requirements.txt`)
+  2. Run this file. (e.g.: `python test.py`)
+  3. Make sure "chromedriver.exe" has been downloaded.
+"""
+import chromedriver_binary_sync
+
+
+chromedriver_binary_sync.download()
